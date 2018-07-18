dacbcc2b897664285d11f7def743c0df491d2455
toanprox7 <37140179+toanprox7@users.noreply.github.com>
2018-07-18 15:29:23 +0700

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://postgres:toanprox7@localhost:5433/postgres');
+var sequelize = new Sequelize('postgres://fcxctktjmxxtdj:9ce4a0d0579f74d3c6fd9ea94985b382fa4de715c0caa1ce1ac1d85767c4852c@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db4np76m4rj0n2');
 
 var User = sequelize.define('users',{
     facebookId:Sequelize.STRING,
